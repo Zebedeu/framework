b87e3038401e56ee95c74bc9ff77b2eeec688f4a
Zebedeu <marciozebedeu@gmail.com>
2019-09-19 20:13:13 +0100

    Revert "update"
    This reverts commit da0a6ce4c852d72c62aa74e64b253ce649e98807.

--- tests/Http/HashTest.php
+++ tests/Http/HashTest.php
@@ -8,41 +8,32 @@
 
 use PHPUnit\Framework\TestCase;
 
-class HashTest extends TestCase
-{
+class HashTest extends TestCase{
 
 
     public function setUp()
     {
 
     }
-
     public function testHash()
     {
-        $hash = \Ballybran\Helpers\Utility\Hash::token(7777);
-        $hash2 = \Ballybran\Helpers\Utility\Hash::token();
-        $this->assertTrue(true , $hash);
-        $this->assertThat(
-        $hash,
-        $this->logicalNot(
-            $this->equalTo($hash2)
-        )
-    );
-
+        $hash = \Ballybran\Helpers\Utility\Hash::token();
+        $this->assertTrue(true, $hash);
     }
 
     public function testIfhash_password()
     {
         $hash = \Ballybran\Helpers\Utility\Hash::hash_password("IT IS TEST");
-        $this->assertTrue(true , $hash);
+        $this->assertTrue(true, $hash);
 
     }
 
-    public function testIfHashverify_password()
-    {
+    public function testIfHashverify_password() {
 
         $hash = \Ballybran\Helpers\Utility\Hash::hash_password("IT IS TEST");
-        $verify = \Ballybran\Helpers\Utility\Hash::verify_password("IT IS TEST" , $hash);
-        $this->assertEquals(true , $verify);
+        $verify = \Ballybran\Helpers\Utility\Hash::verify_password("IT IS TEST", $hash);
+
+        $this->assertEquals(1, $verify);
+
     }
 }
