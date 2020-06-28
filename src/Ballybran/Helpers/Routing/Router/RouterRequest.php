ccf9cab752020f68eec55e70c2fefcfbba4993de
Zebedeu <marciozebedeu@gmail.com>
2020-06-28 08:37:56 +0100

    add a new default routing

---
+++ src/Ballybran/Helpers/Routing/Router/RouterRequest.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ *
+ * KNUT7 K7F (https://marciozebedeu.com/)
+ * KNUT7 K7F (tm) : Rapid Development Framework (https://marciozebedeu.com/)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link      https://github.com/knut7/framework/ for the canonical source repository
+ * @copyright (c) 2015.  KNUT7  Software Technologies AO Inc. (https://marciozebedeu.com/)
+ * @license   https://marciozebedeu.com/license/new-bsd New BSD License
+ * @author    Marcio Zebedeu - artphoweb@artphoweb.com
+ * @version   1.0.14
+ *
+ *
+ */
+
+namespace Ballybran\Routing\Router;
+
+class RouterRequest
+{
+    /**
+     * @var string $validMethods Valid methods for Requests
+     */
+    public static $validMethods = 'GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH|ANY|AJAX|XPOST|XPUT|XDELETE|XPATCH';
+
+    /**
+     * Request method validation
+     *
+     * @param string $data
+     * @param string $method
+     *
+     * @return bool
+     */
+    public static function validMethod($data, $method)
+    {
+        $valid = false;
+        if (strstr($data, '|')) {
+            foreach (explode('|', $data) as $value) {
+                $valid = self::checkMethods($value, $method);
+                if ($valid) {
+                    break;
+                }
+            }
+        } else {
+            $valid = self::checkMethods($data, $method);
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Get the request method used, taking overrides into account
+     *
+     * @return string
+     */
+    public static function getRequestMethod()
+    {
+        // Take the method as found in $_SERVER
+        $method = $_SERVER['REQUEST_METHOD'];
+        // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
+        // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
+        if ($method === 'HEAD') {
+            ob_start();
+            $method = 'GET';
+        } elseif ($method === 'POST') {
+            $headers = self::getRequestHeaders();
+            if (isset($headers['X-HTTP-Method-Override']) &&
+                in_array($headers['X-HTTP-Method-Override'], ['PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'])) {
+                $method = $headers['X-HTTP-Method-Override'];
+            } elseif (! empty($_POST['_method'])) {
+                $method = strtoupper($_POST['_method']);
+            }
+        }
+
+        return $method;
+    }
+
+    /**
+     * check method valid
+     *
+     * @param string $value
+     * @param string $method
+     *
+     * @return bool
+     */
+    protected static function checkMethods($value, $method)
+    {
+        if (in_array($value, explode('|', self::$validMethods))) {
+            if (self::isAjax() && $value === 'AJAX') {
+                return true;
+            }
+
+            if (self::isAjax() && strpos($value, 'X') === 0 && $method === ltrim($value, 'X')) {
+                return true;
+            }
+
+            if (in_array($value, [$method, 'ANY'])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check ajax request
+     *
+     * @return bool
+     */
+    protected static function isAjax()
+    {
+        return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest');
+    }
+
+    /**
+     * Get all request headers
+     *
+     * @return array
+     */
+    protected static function getRequestHeaders()
+    {
+        // If getallheaders() is available, use that
+        if (function_exists('getallheaders')) {
+            return getallheaders();
+        }
+
+        // Method getallheaders() not available: manually extract 'm
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_' || $name === 'CONTENT_TYPE' || $name === 'CONTENT_LENGTH') {
+                $headerKey = str_replace(
+                    [' ', 'Http'],
+                    ['-', 'HTTP'],
+                    ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))
+                );
+                $headers[$headerKey] = $value;
+            }
+        }
+
+        return $headers;
+    }
+}
