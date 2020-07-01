f80a6913dc92a6de49de1ef91e7ab7a4c45a6eb7
Zebedeu <marciozebedeu@gmail.com>
2020-07-01 18:53:38 +0100

    update

--- src/Ballybran/Routing/Router/RouteMiddleware.php
+++ src/Ballybran/Routing/Router/RouteMiddleware.php
@@ -131,4 +131,176 @@ class RouteMiddleware {
         $this->middlewareGroups = $middlewareGroup;
     }
 
+
+    /**
+     * Set paths and namespaces for Controllers and Middlewares.
+     *
+     * @param array $params
+     *
+     * @return void
+     */
+    protected function setPaths($params)
+    {
+        if (empty($params)) {
+            return;
+        }
+
+        if (isset($params['paths']) && $paths = $params['paths']) {
+            $this->paths['controllers'] = isset($paths['controllers'])
+                ? trim($paths['controllers'], '/')
+                : $this->paths['controllers'];
+
+            $this->paths['middlewares'] = isset($paths['middlewares'])
+                ? trim($paths['middlewares'], '/')
+                : $this->paths['middlewares'];
+        }
+
+        if (isset($params['namespaces']) && $namespaces = $params['namespaces']) {
+            $this->namespaces['controllers'] = isset($namespaces['controllers'])
+                ? trim($namespaces['controllers'], '\\') . '\\'
+                : '';
+
+            $this->namespaces['middlewares'] = isset($namespaces['middlewares'])
+                ? trim($namespaces['middlewares'], '\\') . '\\'
+                : '';
+        }
+
+        if (isset($params['base_folder'])) {
+            $this->baseFolder = rtrim($params['base_folder'], '/');
+        }
+
+        if (isset($params['main_method'])) {
+            $this->mainMethod = $params['main_method'];
+        }
+
+        $this->cacheFile = isset($params['cache']) ? $params['cache'] : realpath(__DIR__ . '/../cache.php');
+    }
+
+    /**
+     * @param $controller
+     *
+     * @return RouterException|mixed
+     */
+    protected function resolveClass($controller)
+    {
+        $controller = str_replace(['\\', '.'], '/', $controller);
+        $controller = trim(
+            preg_replace(
+                '/' . str_replace('/', '\\/', $this->paths['controllers']) . '/i',
+                '', $controller,
+                1
+            ),
+            '/'
+        );
+        $file = realpath(rtrim($this->paths['controllers'], '/') . '/' . $controller . '.php');
+
+        if (!file_exists($file)) {
+            return $this->exception($controller . ' class is not found!');
+        }
+
+        $controller = $this->namespaces['controllers'] . str_replace('/', '\\', $controller);
+        if (!class_exists($controller)) {
+            require $file;
+        }
+
+        return $controller;
+    }
+
+    /**
+     * Routes error function. (Closure)
+     *
+     * @param $callback
+     *
+     * @return void
+     */
+    public function error($callback)
+    {
+        $this->errorCallback = $callback;
+    }
+
+    /**
+     * Display all Routes.
+     *
+     * @return void
+     */
+    public function getList()
+    {
+        echo '<pre>';
+        var_dump($this->getRoutes());
+        echo '</pre>';
+        die;
+    }
+
+    /**
+     * Get all Routes
+     *
+     * @return mixed
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Throw new Exception for Router Error
+     *
+     * @param $message
+     *
+     * @return RouterException
+     * @throws
+     */
+    public function exception($message = '')
+    {
+        return new RouterException($message);
+    }
+
+    /**
+     * RouterCommand class
+     *
+     * @return RouterCommand
+     */
+    public function routerCommand()
+    {
+        return RouterCommand::getInstance($this->baseFolder, $this->paths, $this->namespaces);
+    }
+
+    /**
+     * Cache all routes
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function cache()
+    {
+        foreach ($this->getRoutes() as $key => $r) {
+            if (!is_string($r['callback'])) {
+                throw new Exception(sprintf('Routes cannot contain a Closure/Function callback while caching.'));
+            }
+        }
+
+        $cacheContent = '<?php return ' . var_export($this->getRoutes(), true) . ';' . PHP_EOL;
+        if (false === file_put_contents($this->cacheFile, $cacheContent)) {
+            throw new Exception(sprintf('Routes cache file could not be written.'));
+        }
+
+        return true;
+    }
+
+    /**
+     * Load Cache file
+     *
+     * @return bool
+     */
+    protected function loadCache()
+    {
+        if (file_exists($this->cacheFile)) {
+            $this->routes = require $this->cacheFile;
+            $this->cacheLoaded = true;
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
